9c642265cef2eccd4d9536e424fc39013a6562df
whaenth what <whaenth@gmail.com>
2026-03-02 14:42:43 -0600

    created getExhaustTemp function

--- ipmifancurve.sh
+++ ipmifancurve.sh
@@ -8,7 +8,12 @@ read -p "Username: " username
 read -p "Password: " password
 read -p "IP Address: " ipaddress
 
+getExhaustTemp () {
+	exhaustTemp = ipmitool -I lanplus -H $ipaddress -U $username -P $password sdr type temperature | awk 'NR==2 {print $10}'
+}
+
 for i in {1..5};
 do
-	ipmitool -I lanplus -H $ipaddress -U $username -P $password sdr type temperature | awk 'NR==2 {print $10}'
+	getExhaustTemp
+	echo $exhaustTemp
 done
